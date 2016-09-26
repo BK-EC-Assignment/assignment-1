36d2cdc8ad6eab35d872ac7268648bf06fe1e9ea
luongquocdinh <luongquocdinh.95@gmail.com>
2016-09-26 16:00:04 +0700

    header and logo

--- app/components/Main.js
+++ app/components/Main.js
@@ -1,9 +1,11 @@
 var React = require('react');
+var Header = require('./Header');
 
 var Main = React.createClass({
   render: function () {
     return (
       <div className='main-container'>
+        <Header />
         {this.props.children}
       </div>
     )
